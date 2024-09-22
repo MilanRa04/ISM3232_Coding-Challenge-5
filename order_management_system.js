a374e4faf59916c5165e982204cdfc1d4bd5b544
Milan Ranpariya <ranpariya04@gmail.com>
2024-09-22 14:22:02 -0400

    Initialize inventory with product objects

---
+++ order_management_system.js
@@ -0,0 +1,7 @@
+// Create an array with the 4 product objects, and include name, price, and quantity.
+const inventory = [
+    {name: 'Espresso', price: 3, quantity: 10 },
+    {name: 'Latte', price: 4, quantity: 5 },
+    {name: 'Cappuccino', prince: 4, quantity: 6 },
+    {name: 'Mocha', price: 5, quantity: 4 }
+];
