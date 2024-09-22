50bd9036127807e93bfddbc8d03fb6ef3c7a66bf
Milan Ranpariya <ranpariya04@gmail.com>
2024-09-22 16:23:07 -0400

    Initialize orders array

--- order_management_system.js
+++ order_management_system.js
@@ -1,7 +1,14 @@
-// Create an array with the 4 product objects, and include name, price, and quantity.
+// Task 1: Create an array with the 4 product objects, and include name, price, and quantity.
 const inventory = [
     {name: 'Espresso', price: 3, quantity: 10 },
     {name: 'Latte', price: 4, quantity: 5 },
     {name: 'Cappuccino', prince: 4, quantity: 6 },
     {name: 'Mocha', price: 5, quantity: 4 }
 ];
+
+
+// Task 2: Create an empty order array of order objects.
+const orders = [];
+
+// orders.push = { name: 'John', item: 'Espresso', quantity: 1, status: 'complete' }
+// console.log(orders)
